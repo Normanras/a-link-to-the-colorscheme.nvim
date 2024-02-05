fe41deb76fb23e8e8fcbb3a38fff57014230375c
Norm Rasmussen <norman.rasmussen@me.com>
2024-02-05 10:24:18 -0500

    util.lua fixes to add functions for overwriting colorscheme.

--- lua/link/util.lua
+++ lua/link/util.lua
@@ -31,4 +31,52 @@ function util.load()
     theme.loadTerminal()
 end
 
+---@param config Config
+function util.autocmds(config)
+  local group = vim.api.nvim_create_augroup("link", { clear = true })
+
+    vim.api.nvim_create_autocmd("ColorSchemePre", {
+    group = group,
+    callback = function()
+      vim.api.nvim_del_augroup_by_id(group)
+    end,
+  })
+  local function set_whl()
+    local win = vim.api.nvim_get_current_win()
+    local whl = vim.split(vim.wo[win].winhighlight, ",")
+    vim.list_extend(whl, { "Normal:NormalSB", "SignColumn:SignColumnSB" })
+    whl = vim.tbl_filter(function(hl)
+      return hl ~= ""
+    end, whl)
+    vim.opt_local.winhighlight = table.concat(whl, ",")
+  end
+
+  vim.api.nvim_create_autocmd("FileType", {
+    group = group,
+    pattern = table.concat(config.sidebars, ","),
+    callback = set_whl,
+  })
+  if vim.tbl_contains(config.sidebars, "terminal") then
+    vim.api.nvim_create_autocmd("TermOpen", {
+      group = group,
+      callback = set_whl,
+    })
+  end
+end
+
+---@param theme Theme
+function util.load(theme)
+  -- only needed to clear when not the default colorscheme
+  if vim.g.colors_name then
+    vim.cmd("hi clear")
+  end
+
+  vim.o.termguicolors = true
+  vim.g.colors_name = "link"
+
+  util.autocmds(theme.config)
+
+end
+
+
 return util
