10d4759d4dbf02bfeba374ab7a2eaae7846a6f6a
Norm Rasmussen <norman.rasmussen@me.com>
2024-02-05 10:19:35 -0500

    _load() returned a nil value. Updated the tbl_deep_extend.

--- lua/link/init.lua
+++ lua/link/init.lua
@@ -1,11 +1,31 @@
 local util = require("link.util")
+local theme = require("link.theme")
+local config = require("link.config")
 
 local M = {}
 
-function M.setup()
-    util.load()
+function M._load(style)
+  if style and not M._style then
+    M._style = require("link.config").options.style
+  end
+  if not style and M._style then
+    require("link.config").options.style = M._style
+    M._style = nil
+  end
+  M.load({ style = style, use_background = style == nil })
 end
 
-M.setup()
+---@param opts Config|nil
+function M.load(opts)
+  if opts then
+    require("link.config").extend(opts)
+  end
+  util.load(theme.setup())
+end
+
+M.setup = config.setup
+
+-- keep for backward compatibility
+M.colorscheme = M.load
 
 return M
