126b7c3174e410d966fa5e07b4b6b9358603c3e1
Norm Rasmussen <norman.rasmussen@me.com>
2024-02-05 10:51:14 -0500

    Back to tol defaults with link's colors....

--- lua/link/init.lua
+++ lua/link/init.lua
@@ -1,31 +1,12 @@
+
 local util = require("link.util")
-local theme = require("link.theme")
-local config = require("link.config")
 
 local M = {}
 
-function M._load(style)
-  if style and not M._style then
-    M._style = require("link.config").options.style
-  end
-  if not style and M._style then
-    require("link.config").options.style = M._style
-    M._style = nil
-  end
-  M.load({ style = style, use_background = style == nil })
+function M.setup()
+    util.load()
 end
 
----@param opts Config|nil
-function M.load(opts)
-  if opts then
-    require("link.config").extend(opts)
-  end
-  util.load(theme.setup())
-end
-
-M.setup = config.setup
-
--- keep for backward compatibility
-M.colorscheme = M.load
+M.setup()
 
 return M
