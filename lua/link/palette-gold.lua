35a99cddeff2a73f0ec674385f984b26f98fb8b9
Norm Rasmussen <norman.rasmussen@me.com>
2024-02-05 12:00:02 -0500

    Color switches

---
+++ lua/link/palette-gold.lua
@@ -0,0 +1,28 @@
+---@class Palette
+return {
+   bg = "#b59d57",
+   fg = "#F8F8F2",
+   selection = "#100a38",
+   comment = "#445ea0",
+   red = "#d90000",
+   orange = "#e07e07",
+   yellow = "#e9b806",
+   green = "#683929",
+   purple = "#9ab5e0",
+   cyan = "#b6dff1",
+   pink = "#a00201",
+   bright_red = "#db6b6c",
+   bright_green = "#49c949",
+   bright_yellow = "#efe397",
+   bright_blue = "#76a0d1",
+   bright_magenta = "#FF92DF",
+   bright_cyan = "#A4FFFF",
+   bright_white = "#FFFFFF",
+   menu = "#d5effb",
+   visual = "#3E4452",
+   gutter_fg = "#4B5263",
+   nontext = "#3B4048",
+   white = "#ABB2BF",
+   black = "#191A21",
+}
+
